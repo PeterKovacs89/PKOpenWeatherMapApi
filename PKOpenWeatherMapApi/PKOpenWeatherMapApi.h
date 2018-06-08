7f971b8d51356aa5a24daa1a5cc09a5dbee4f2e4
PeterKovacs <kope8899@gmail.com>
2018-06-08 11:15:49 +0200

    Initial Commit

---
+++ PKOpenWeatherMapApi/PKOpenWeatherMapApi.h
@@ -0,0 +1,19 @@
+//
+//  PKOpenWeatherMapApi.h
+//  PKOpenWeatherMapApi
+//
+//  Created by PeterKovacs on 2018. 06. 08..
+//  Copyright © 2018. PeterKovacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PKOpenWeatherMapApi.
+FOUNDATION_EXPORT double PKOpenWeatherMapApiVersionNumber;
+
+//! Project version string for PKOpenWeatherMapApi.
+FOUNDATION_EXPORT const unsigned char PKOpenWeatherMapApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKOpenWeatherMapApi/PublicHeader.h>
+
+
